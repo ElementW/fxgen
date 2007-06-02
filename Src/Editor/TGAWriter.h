341c95b11c34312401b9fe047685e054215f41c0
dentoid <Dentoid@users.noreply.github.com>
2007-06-02 15:41:37 +0000

    Added a TGA export option to the viewport context menu.

---
+++ Src/Editor/TGAWriter.h
@@ -0,0 +1,19 @@
+//-----------------------------------------------------------------
+//-----------------------------------------------------------------
+//! \file		TGAWriter.h
+//! \brief	Main Frame
+//!
+//!	\author	Anders Stenberg (anders.stenberg@gmail.com)
+//!	\date		02-06-2007
+//!
+//!	\brief	This file applies the GNU GENERAL PUBLIC LICENSE
+//!					Version 2, read file COPYING.
+//!
+//!    			The original version of this library can be located at:
+//!    			http://sourceforge.net/projects/fxgen/
+//!
+//-----------------------------------------------------------------
+//-----------------------------------------------------------------
+#pragma once
+
+void WriteTGA(NBitmap* _bmp, NString _path = "", NString _suffix = "");
